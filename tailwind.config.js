dc21a392cb5b0dabe986769eb4a292a988733dc7
agent109@site69.ru <agent109@ugxrcwltbg>
2023-05-22 09:19:05 +0000

    feat: add menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["./index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateRows: {
+				'menu': "repeat(9, minmax(64px, auto))",
+			},
+			boxShadow: {
+				"navigation-bar":
+					"0 0 4px 0 rgba(0,0,0,.1), 0 -10px 15px rgba(0,0,0,.08)",
+				'menu': "0 7px 14px rgba(11,31,53,.1)",
+				"round-button": "0px 5px 5px -5px rgba(48, 59, 67, 1) inset;",
+			},
+		},
+		fontFamily: {
+			'oswald': ["Oswald", "sans-serif"],
+		},
 	},
 	plugins: [],
 };
